4cd9b750c75e13482fcf29789901f39a94daa343
Robert Simai <robert.simai@suse.com>
2015-12-09 17:29:12 +0100

    something working

---
+++ array.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# rsimai@suse.com
+
+import time
+
+# dimensions
+rows = 15
+cols = 25
+
+ship = 'X'
+noship = ' '
+fillup = 1.3
+fillrows = 5
+
+# rotate 1 is right, -1 is left
+rotate = 1
+
+z=[]
+
+# output in cols x rows
+def printout():
+    for a in range(0,rows):
+        #print a,
+        for b in range(0,cols):
+            print z[((cols)*a+b)],
+        print
+
+# create a list
+def create():
+    for a in range(0,rows):
+        for b in range(0,cols):
+            #print a,b
+            if (a < fillrows) and (b/fillup == int(b/fillup)):
+                z.append(ship)
+            else:
+                z.append(noship)
+
+
+def check_out():
+    for a in range(0,rows):
+        pos1 = a*cols+cols-1
+        pos2 = a*cols
+        #print pos2, pos1
+        if ((z[pos1] == ship) and (rotate == 1)) or ((z[pos2] == ship) and (rotate == -1)):
+            #print "add a line"
+            global rotate
+            rotate = rotate * -1
+            add_line()
+            return
+    if rotate == 1:
+        move_right()
+    else:
+        move_left()
+
+def move_right():
+    global z
+    z.insert(0,noship)
+    z.pop(rows*cols-1)
+
+def move_left():
+    global z
+    z.pop(0)
+    z.append(noship)
+
+def add_line():
+    #print "add line"
+    for a in range(0,cols):
+        global z
+        z.insert(0,noship)
+
+# main
+create()
+
+for i in range(0,50):
+    printout()
+    check_out()
+    time.sleep(0.5)
+    #print rotate
