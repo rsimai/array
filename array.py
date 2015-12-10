03c8db09fa44560f5e0460e1b75d7f6237246f83
Robert Simai <robert.simai@suse.com>
2015-12-10 10:56:52 +0100

    board ready, now for the keys and movement ...

--- array.py
+++ array.py
@@ -7,11 +7,16 @@ import time
 import os
 
 # dimensions
-rows = 15
+rows = 10
 cols = 45
 
+# characters to be used
 ship = 'X'
 noship = '.'
+defender = 'U'
+shot = '|'
+
+# automatic fill up
 fillup = 1.3
 fillrows = 5
 
@@ -45,59 +50,71 @@ def print_defender():
 def create_defender():
     for a in range(0,cols-1):
         y.append(' ')
-    y.insert(cols/2,'U')
+    y.insert(cols/2,defender)
     
 # create a list
 def create_matrix():
     for a in range(0,rows):
         for b in range(0,cols):
-            #print a,b
             if (a < fillrows) and (b/fillup == int(b/fillup)):
                 z.append(ship)
             else:
                 z.append(noship)
 
-
+# check if left or right has been reached, add a line and change direction
 def check_out():
     for a in range(0,rows):
         pos1 = a*cols+cols-1
         pos2 = a*cols
-        #print pos2, pos1
         if ((z[pos1] == ship) and (rotate == 1)) or ((z[pos2] == ship) and (rotate == -1)):
-            #print "add a line"
             global rotate
             rotate = rotate * -1
-            add_line()
+            move_down()
             return
     if rotate == 1:
         move_right()
     else:
         move_left()
 
+# step to the right
 def move_right():
     global z
     z.insert(0,noship)
     z.pop(rows*cols-1)
 
+# step to the left
 def move_left():
     global z
     z.pop(0)
     z.append(noship)
 
-def add_line():
-    #print "add line"
+# move down by one line
+def move_down():
     for a in range(0,cols):
         global z
         z.insert(0,noship)
+        z.pop(cols*rows)
+
+# check end
+def check_end():
+    if z[cols*rows-1] == ship:
+        print "Game over!"
+        exit(0)
 
 # main
+
+# prepare
 create_defender()
 create_matrix()
 
-# play
-for i in range(0,50):
+# play loop
+for i in range(0,90):
+    # printing/checking
     os.system('clear')
+    check_out()
     printout()
     print_defender()
-    check_out()
-    time.sleep(0.3)
+    check_end()
+
+    # waiting
+    time.sleep(0.1)
